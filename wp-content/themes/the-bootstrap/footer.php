d72cdf4528f2178a415fa842e146df1f3a77a462
Pesnya Tatiana <tatianapesnya@gmail.com>
2013-03-21 14:45:29 +0100

    Supression dossier du theme fashionlab

--- wp-content/themes/the-bootstrap/footer.php
+++ wp-content/themes/the-bootstrap/footer.php
@@ -6,9 +6,37 @@
  */
 				tha_footer_before(); ?>
 				<footer id="colophon" role="contentinfo" class="span12">
-					<?php tha_footer_top(); ?>
 					<div id="page-footer" class="well clearfix">
+					<?php tha_footer_top(); ?>
 						<div class="span3">
+							<h1 id="site-title">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+								<span><img src="<?php bloginfo('template_directory'); ?>/img/fashionlab-gray.png" alt="<?php bloginfo('name'); ?>" /></span>
+							</a>
+						</h1>
+					</div>
+					<div class="span3">
+						<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?>       
+            
+       					 <?php endif; ?>                
+					</div><!--span3-->
+					<div class="span3">
+						<h2><a href="#">Stylists</a></h2>
+							<ul>
+								<li><a href="#">François Quentin</a></li>
+								<li><a href="#">Jonathan Riss</a></li>
+								<li><a href="#">Julien Fournié</a></li>
+							</ul>
+					</div>
+					<div class="span2">
+						<h2><a href="#">Partners</a></h2>
+							<ul>
+								<li><a href="#">Media</a></li>
+								<li><a href="#">Technologic</a></li>
+							</ul>
+					</div>
+					</div><!-- #page-footer .well .clearfix -->
+					<div class="span12">	
 						<?php wp_nav_menu( array(
 							'container'			=>	'nav',
 							'container_class'	=>	'subnav',
@@ -20,16 +48,6 @@
 						) );
 						?>
 					</div>
-					<div class="span3">
-						<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?>       
-            
-       					 <?php endif; ?>                
-					</div>
-					<div class="span3">
-					</div>
-					<div class="span3">
-					</div>
-					</div><!-- #page-footer .well .clearfix -->
 					<?php tha_footer_bottom(); ?>
 				</footer><!-- #colophon -->
 				<?php tha_footer_after(); ?>
