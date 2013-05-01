992cce39d8136f841ef74dfbd40c7ab5d6fbd0e3
Joel <jleger@majes.fr>
2013-05-01 17:42:39 +0200

    header 3DS

--- wp-content/themes/the-bootstrap/js/header-3ds.js
+++ wp-content/themes/the-bootstrap/js/header-3ds.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($) {
 	var firstMenuHide = setTimeout(function(){$('#header_drag .header_drag_toggle').trigger('click')}, 4000);
 	
-	clearTimeout(firstMenuHide);
-	$('#header_3ds').css('height', '1px');
+	//clearTimeout(firstMenuHide);
+	//$('#header_3ds').css('height', '1px');
 
 	$(document).on('click', '#header_drag .header_drag_toggle', function(){
 				
